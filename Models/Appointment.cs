6f2236c393de621c068f60163fc757db838997a7
Victor Nunes <nunes.victoramaral@gmail.com>
2024-06-25 22:44:09 -0300

    create appointment model

---
+++ Models/Appointment.cs
@@ -0,0 +1,22 @@
+﻿namespace Barbershop.API.Models
+{
+    public class Appointment
+    {
+        public Appointment(DateTime schedule, Barber barber, Client client)
+        {
+            Schedule = schedule;
+            Barber = barber;
+            Client = client;
+            CreatedAt = DateTime.Now;
+            
+        }
+
+        public int Id { get; private set; }
+        public DateTime Schedule { get; private set; }
+        public Barber Barber { get; private set; }
+        public Client Client { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime UpdatedAt { get; private set; }
+
+    }
+}
