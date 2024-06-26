b06ba3442940daa2f77120986db6889043585fe0
Victor Nunes <nunes.victoramaral@gmail.com>
2024-06-25 22:43:13 -0300

    create client model

---
+++ Models/Client.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Barbershop.API.Models
+{
+    public class Client
+    {
+        public Client(string firstName, string lastName, string email, string phone)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Phone = phone;
+            CreatedAt = DateTime.Now;
+        }
+        public int Id { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set;}
+        public string Email { get; private set; }
+        public string Phone { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime UpdatedAt { get; private set; }
+    }
+}
