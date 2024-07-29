a0a0be011f3def400a6ea77e6c4fea7f961062ab
Victor Nunes <nunes.victoramaral@gmail.com>
2024-07-29 19:57:36 -0300

    add appointment mapping

---
+++ Data/Mappings/AppointmentMap.cs
@@ -0,0 +1,38 @@
+﻿using Barbershop.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Barbershop.API.Data.Mappings
+{
+    public class AppointmentMap : IEntityTypeConfiguration<Appointment>
+    {
+        public void Configure(EntityTypeBuilder<Appointment> builder)
+        {
+            builder.ToTable("Appointment");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            builder.Property(x => x.Schedule)
+                .IsRequired()
+                .HasColumnType("DATETIME");
+
+            builder.HasOne(x => x.Barber)
+                .WithMany(x => x.Appointments)
+                .HasConstraintName("FK_Appointments_Barber");
+
+            builder.HasOne(x => x.Client)
+                .WithMany(x => x.Appointments)
+                .HasConstraintName("FK_Appointments_Client");
+
+
+            builder.Property(x => x.CreatedAt)
+                .HasColumnType("DATETIME");
+            builder.Property(x => x.UpdatedAt)
+                .HasColumnType("DATETIME");
+        }
+    }
+}
