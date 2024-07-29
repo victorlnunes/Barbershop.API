847200b7db8c0f76d989b6c2d5d3d4488589e04d
Victor Nunes <nunes.victoramaral@gmail.com>
2024-07-29 19:33:28 -0300

    add foringkey on appoitment model

--- Models/Appointment.cs
+++ Models/Appointment.cs
@@ -11,7 +11,9 @@
         }
         public int Id { get; private set; }
         public DateTime Schedule { get; private set; }
+        public int BarberId { get; private set; }
         public virtual Barber Barber { get; private set; }
+        public int ClientId { get; set; }
         public virtual Client Client { get; private set; }
         public DateTime CreatedAt { get; private set; }
         public DateTime UpdatedAt { get; private set; }
