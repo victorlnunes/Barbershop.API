b7be8618fb373dace7e9463ad1bd4794882ec3f3
Victor Nunes <nunes.victoramaral@gmail.com>
2024-08-12 21:45:51 -0300

    add migration of change on barber table

---
+++ Migrations/20240808002929_UpdateBioColumnOnBarberTableToAcceptLongText.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Barbershop.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBioColumnOnBarberTableToAcceptLongText : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Bio",
+                table: "Barber",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Bio",
+                table: "Barber",
+                type: "VARCHAR",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
