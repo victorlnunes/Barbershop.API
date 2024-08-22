20060515aae20bfbc04a8292cad72fc3b0a5b0a0
Victor Nunes <nunes.victoramaral@gmail.com>
2024-08-21 22:28:02 -0300

    adds swagger

--- Program.cs
+++ Program.cs
@@ -12,8 +12,13 @@ builder.Services.AddControllers();
 builder.Services.AddMemoryCache();
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.MapControllers();
 
